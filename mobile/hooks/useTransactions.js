da6dd6e8da630a30276f47008006c8a138b04d80
Joseph Ameh <joeoameh@gmail.com>
2025-08-11 18:57:29 +0100

    Restucture folder

--- mobile/hooks/useTransactions.js
+++ mobile/hooks/useTransactions.js
@@ -2,7 +2,7 @@ import { useCallback, useState } from "react";
 import { Alert } from "react-native";
 
 const API_URL = "http://10.140.122.231:5001/api";
-
+// const API_URL = "http://localhost:5001/api"
 export const useTransactions = (userId) => {
   const [transactions, setTransactions] = useState([]);
   const [summary, setSummary] = useState({
